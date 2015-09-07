9569ffdcdcfebc190724fc1932e52bef66854553
ruwhan <ruwhan@outlook.com>
2015-09-08 06:13:16 +0700

    make the test green

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::UsersController < Api::V1::BaseApiController
+  before_action :authenticate_with_token!, :only => [:update, :destroy]
+  # respond_to :json
+
+  def show 
+    render :json => { :user => User.find(params[:id]) } 
+  end
+
+  def create 
+    user = User.new(user_params)
+    if user.save
+      render :json => { user: user }, status: 201, location: [:v1, user]
+    else
+      render :json => { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = current_user
+    # user = User.find(params[:id])
+
+    if user.update(user_params)
+      render :json => { user: user }, status: 200, location: [:v1, user]
+    else
+      render :json => { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    current_user.destroy
+
+    head 204
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  scope module: :api do 
+    namespace :v1, defaults: { format: :json } do 
+      devise_for :users
+
+      resources :users
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
