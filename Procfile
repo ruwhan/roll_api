3c9d3d8132ce5cde37631d436b8957a0cda63048
ruwhan <ruwhan@outlook.com>
2015-09-08 06:01:26 +0700

    add UsersController and its test spec

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: bundle exec rails server -p $PORT
 worker: bundle exec sidekiq
 redis: redis-server
+elasticsearch: elasticsearch
--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -5,5 +5,56 @@ class Authentication < ActionController::Base
 end
 
 describe Authenticable do 
-  
+  let(:authentication) { Authentication.new }
+  subject { authentication }
+
+  describe "#current_user" do 
+    before do 
+      @user = FactoryGirl.create :user
+      request.headers["Authorization"] = @user.auth_token
+      #allow(authentication).to receive(:request).and_return(request)
+      authentication.stub(:request).and_return(request)
+    end
+
+    it "returns the user from the authorization header" do 
+      expect(authentication.current_user.auth_token).to eql @user.auth_token
+    end
+  end
+
+  describe "#authenticate_with_token" do 
+    before do 
+      @user = FactoryGirl.create :user
+      authentication.stub(:current_user).and_return(nil)
+      response.stub(:response_code).and_return(401)
+      response.stub(:body).and_return({ "errors" => "Not authenticated" }.to_json)
+      authentication.stub(:response).and_return(response)
+    end
+
+    it "render a json error message" do 
+      errors_response = JSON.parse(response.body, symbolize_names: true)
+      expect(errors_response[:errors]).to eql "Not authenticated"
+    end
+
+    it { should respond_with 401 }
+  end
+
+  describe "#user_signed_in?" do 
+    context "when there is on 'session'" do 
+      before do 
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "when there is no user on 'session'" do 
+      before do 
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(nil)
+      end
+
+      it { should_not be_user_signed_in }
+    end
+  end
 end
