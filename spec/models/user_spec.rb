70e0634ed6e1b41708564764f756ae586a3127ee
ruwhan <ruwhan@outlook.com>
2015-09-08 06:36:06 +0700

    auth_token uniqueness validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
+  it { should validate_uniqueness_of(:auth_token) }
 
   describe "#generate_authentication_token!" do 
     it "generates_authentication_token" do 
